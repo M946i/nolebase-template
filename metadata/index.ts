f969c2157695ecd95b858e341c9cdcb10b15572d
M946i <484616632@qq.com>
2024-07-25 20:09:29 +0800

    update

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,17 +1,17 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
+export const siteName = 'Blog'
+export const siteShortName = 'Blog'
 export const siteDescription = '记录回忆，知识和畅想的地方'
 
 /** 文档所在目录 */
 export const include = ['笔记', '生活']
 
 /** Repo */
-export const githubRepoLink = 'https://github.com/nolebase/nolebase'
+export const githubRepoLink = 'https://github.com/M946i'
 /** Discord */
-export const discordLink = 'https://discord.gg/XuNFDcDZGj'
+export const discordLink = 'https://discord.gg/nXkYY88d'
 
 /** 无协议前缀域名 */
-export const plainTargetDomain = 'nolebase.ayaka.io'
+export const plainTargetDomain = 'blog-m946is.vercel.app/'
 /** 完整域名 */
 export const targetDomain = `https://${plainTargetDomain}`
